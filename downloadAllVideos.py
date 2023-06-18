ebed1758306709dc567b05e8d9ca8e1608b77439
suellenlima <37180275+suellenlima@users.noreply.github.com>
2023-06-18 01:45:43 -0300

    choro: get videos in progressive array too

--- downloadAllVideos.py
+++ downloadAllVideos.py
@@ -25,11 +25,10 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
         (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
     'Referer': 'https://alunos.aprendavfx.com/auth/login?redirect=/area/produto/item/885648',
-    'Cookie': '_ga=GA1.1.1431774022.1686544285; laravel_session=XuM7ZGtR0OCD6E3VlzuWbdR5AyLRuK4UdpNjGfi6; __cflb=02DiuH5Ncy7aKdUJtG1UW5cn2bVRqKEmRVAPhDrpsTNw6; _ga_37GXT4VGQK=GS1.1.1686975279.25.1.1686976176.0.0.0; XSRF-TOKEN=eyJpdiI6IlBRZWtSUWVUeWlNcXo2eDZwSWpoeXc9PSIsInZhbHVlIjoiT0lyNVN6d3BvUUZMVTN4aU56QzZKQkJqU3VKVDNTREFyaDJLMkdJSUJjZ3hjZERXVXhQQlpSa2VJdktaM3k0eWpoeWpkaTU2dlcwSzExYThHeTQ1Rnp5c3VDNVBjTm9GeE90YjNxQkhZZTFaT0E0RzlJYzRGVlk3d004TkNTeUYiLCJtYWMiOiI4MDUyZjY0NDc2OGJkOGJmZWZhMTdiNjZjZTE4NmJlOGQ4MmU3NmJiYTlmZmQzMTMyODE4MjhjY2Q2MzdjMjZiIn0%3D'
+    'Cookie': '_ga=GA1.1.1431774022.1686544285; laravel_session=8jOAvN8odpy5Go2oCqrHWZA8SEBQmjPTzXBtmnk8; __cflb=02DiuH5Ncy7aKdUJtG1UW5cn2bVRqKEmSfXCQzeRkEMbA; _ga_37GXT4VGQK=GS1.1.1687060365.27.1.1687060366.0.0.0; XSRF-TOKEN=eyJpdiI6Ii9iZG8wMkdWSEhjcHlmaDRzS05ENXc9PSIsInZhbHVlIjoiMHREcXg4WGQ0VENkc1dxUHJFRWZkYlN6WHg4TUFLK3NJeXB2eWNHUjYrTW9pVnIyOVpiNUoyNC85dzFvUERaaDhKbjJyQWhKcmowUitaOXgzZWc1cHpGZllyUVRmeUxONktHSDFEbVVPSkFkelZvaWU5WHVMWUZ3U29vRFBmZDYiLCJtYWMiOiI3ZWRhZmNkYmU5OTM1MzZjZDcxNjEzYmJhY2YxZDc2Nzg1OTc2Mjc0ODJiZjBiOGEyYmQ1MmIyZDIyMTE1ZjVkIn0%3D'
 }
   
 # Get the page through get() method
-# html_response = requests.get(url=url, headers = headers)
 html_response = requests.get(url=url, headers=headers)
   
 # Save the page content as sample.html
@@ -44,7 +43,7 @@ soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
 
 # Buscar os valores da propriedade data-item-id
 ids = [div['data-item-id'] for div in soup.find_all('div', {'data-item-id': True})]
-
+testids = [ids[4], ids[5], ids[6], ids[7]]
 # Imprimir a lista de valores
 print(ids)
 
@@ -65,10 +64,11 @@ with open('valores.txt', 'w', encoding='utf-8') as arquivo:
 
 # Imprimir a lista de valores
 print(valores)
+testvalores = [valores[4], valores[5], valores[6], valores[7]]
 
 int = 0
 
-for id in ids:
+for id in testids:
     html_response = requests.get(url=urlDefault+id, headers=headers)
     
     # Analisar o HTML usando BeautifulSoup
@@ -111,40 +111,73 @@ for id in ids:
 
     # Extrair o valor do primeiro "avc_url" do JSON
     player_config = json.loads(json_content)
-    avc_urls = player_config['request']['files']['dash']['cdns']['akfire_interconnect_quic']
-    print(avc_urls)
-    primeiro_avc_url = avc_urls['avc_url']
-    primeiro_avc_url = str(primeiro_avc_url)
-    # Imprimir o valor encontrado
-    print(primeiro_avc_url)
+    progressive = player_config['request']['files']['progressive']
+    hasProgressive = True if len(progressive) else False
+    if hasProgressive:
+        progressiveUrl = ''
+        for objeto in progressive:
+            # Verificar se o valor desejado está presente
+            if objeto["width"] == 1280:
+                progressiveUrl = objeto["url"]
+                break
+        if progressiveUrl == '':
+            lastIndex = len(progressive)
+            test = progressive[lastIndex]
+            print(test)
+            progressiveUrl = test["url"]
+        print(progressiveUrl)
+        url = progressiveUrl
+        url = str(url)
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(testvalores[int], 'wb') as arquivo:
+                for chunk in response.iter_content(chunk_size=1024):
+                    arquivo.write(chunk)
+        int = int + 1
+    else:
+        avc_urls = player_config['request']['files']['dash']['cdns']['akfire_interconnect_quic']
+        print(avc_urls)
+        url = avc_urls['avc_url']
+        url = str(url)
+        # Imprimir o valor encontrado
+        print(url)
 
-    # Extract some stuff
-    base_url = primeiro_avc_url[:primeiro_avc_url.rfind('/', 0, -26) + 1]
-    resp = requests.get(primeiro_avc_url)
-    content = resp.json()
+        # Extract some stuff
+        base_url = url[:url.rfind('/', 0, -26) + 1]
+        resp = requests.get(url)
+        content = resp.json()
 
-    # Fix the base url
-    base_url = urllib.parse.urljoin(base_url, content['base_url'])
-    
-    # Video download here
-    heights = [(i, d['height']) for (i, d) in enumerate(content['video'])]
-    idx = max(heights, key=lambda x: x[1])[0]
-    video = content['video'][idx]
-    video_base_url = base_url + video['base_url']
-    print('Base url:', video_base_url)
+        # Fix the base url
+        base_url = urllib.parse.urljoin(base_url, content['base_url'])
+        
+        # Video download here
+        heights = [(i, d['height']) for (i, d) in enumerate(content['video'])]
+        idx = max(heights, key=lambda x: x[1])[0]
+        video = content['video'][idx]
+        video_base_url = base_url + video['base_url']
+        print('Base url:', video_base_url)
 
-    filenameVideo = 'video_%s.mp4' % video['id']
-    print('Saving VIDEO to %s' % filenameVideo)
+        filenameVideo = 'video_%s.mp4' % video['id']
+        print('Saving VIDEO to %s' % filenameVideo)
 
-    video_file = open(filenameVideo, 'wb')
+        video_file = open(filenameVideo, 'wb')
 
-    init_segment = base64.b64decode(video['init_segment'])
-    video_file.write(init_segment)
-    
-    if init_segment:
-        for segment in tqdm(video['segments']):
-            segment_url = video_base_url + segment['url']
-            resp = requests.get(segment_url, stream=True)
+        init_segment = base64.b64decode(video['init_segment'])
+        video_file.write(init_segment)
+        
+        if init_segment:
+            for segment in tqdm(video['segments']):
+                segment_url = video_base_url + segment['url']
+                resp = requests.get(segment_url, stream=True)
+                if resp.status_code != 200:
+                    print('not 200!')
+                    print(resp)
+                    print(segment_url)
+                    break
+                for chunk in resp:
+                    video_file.write(chunk)
+        else:
+            resp = requests.get(video_base_url, stream=True)
             if resp.status_code != 200:
                 print('not 200!')
                 print(resp)
@@ -152,40 +185,42 @@ for id in ids:
                 break
             for chunk in resp:
                 video_file.write(chunk)
-    else:
-        resp = requests.get(video_base_url, stream=True)
-        if resp.status_code != 200:
-            print('not 200!')
-            print(resp)
-            print(segment_url)
-            break
-        for chunk in resp:
-            video_file.write(chunk)
 
-    video_file.flush()
-    video_file.close()
+        video_file.flush()
+        video_file.close()
 
-    # Audio download here
-    bitrate = [(i, d['bitrate']) for (i, d) in enumerate(content['audio'])]
+        # Audio download here
+        bitrate = [(i, d['bitrate']) for (i, d) in enumerate(content['audio'])]
 
-    print('Bitrate', bitrate)
+        print('Bitrate', bitrate)
 
-    idx = max(bitrate, key=lambda x: x[1])[0]
-    audio = content['audio'][idx]
-    audio_base_url = base_url + audio['base_url']
-    print('Base url:', audio_base_url)
+        idx = max(bitrate, key=lambda x: x[1])[0]
+        audio = content['audio'][idx]
+        audio_base_url = base_url + audio['base_url']
+        print('Base url:', audio_base_url)
 
-    filenameAudio = 'audio_%s.mp4' % audio['id']
-    print('Saving AUDIO to %s' % filenameAudio)
+        filenameAudio = 'audio_%s.mp4' % audio['id']
+        print('Saving AUDIO to %s' % filenameAudio)
 
-    audio_file = open(filenameAudio, 'wb')
+        audio_file = open(filenameAudio, 'wb')
 
-    init_segment = base64.b64decode(audio['init_segment'])
-    audio_file.write(init_segment)
-    
-    if init_segment:
-        for segment in tqdm(audio['segments']):
-            segment_url = audio_base_url + segment['url']
+        init_segment = base64.b64decode(audio['init_segment'])
+        audio_file.write(init_segment)
+        
+        if init_segment:
+            for segment in tqdm(audio['segments']):
+                segment_url = audio_base_url + segment['url']
+                segment_url = re.sub(r'/[a-zA-Z0-9_-]*/\.\./',r'/',segment_url.rstrip())
+                resp = requests.get(segment_url, stream=True)
+                if resp.status_code != 200:
+                    print('not 200!')
+                    print(resp)
+                    print(segment_url)
+                    break
+                for chunk in resp:
+                    audio_file.write(chunk)
+        else:
+            segment_url = audio_base_url
             segment_url = re.sub(r'/[a-zA-Z0-9_-]*/\.\./',r'/',segment_url.rstrip())
             resp = requests.get(segment_url, stream=True)
             if resp.status_code != 200:
@@ -195,38 +230,27 @@ for id in ids:
                 break
             for chunk in resp:
                 audio_file.write(chunk)
-    else:
-        segment_url = audio_base_url
-        segment_url = re.sub(r'/[a-zA-Z0-9_-]*/\.\./',r'/',segment_url.rstrip())
-        resp = requests.get(segment_url, stream=True)
-        if resp.status_code != 200:
-            print('not 200!')
-            print(resp)
-            print(segment_url)
-            break
-        for chunk in resp:
-            audio_file.write(chunk)
 
-    audio_file.flush()
-    audio_file.close()
+        audio_file.flush()
+        audio_file.close()
 
-    # Combine audio and video here
-    print('Combining video and audio...')
-    cmd = 'ffmpeg -y -i '
-    cmd += filenameAudio
-    cmd += ' -i '
-    cmd += filenameVideo
-    cmd += ' ' + valores[int]
-    subprocess.call(cmd, shell=True)
-    print('Mixing Done!')
-    
-    int = int + 1
+        # Combine audio and video here
+        print('Combining video and audio...')
+        cmd = 'ffmpeg -y -i '
+        cmd += filenameAudio
+        cmd += ' -i '
+        cmd += filenameVideo
+        cmd += ' ' + valores[int]
+        subprocess.call(cmd, shell=True)
+        print('Mixing Done!')
+        
+        int = int + 1
 
-    # Delete the remaining single audio and video files
-    os.remove("video.html")
-    os.remove(filenameAudio)
-    os.remove(filenameVideo)
-    print("Temporary files removed!")
+        # Delete the remaining single audio and video files
+        os.remove("video.html")
+        os.remove(filenameAudio)
+        os.remove(filenameVideo)
+        print("Temporary files removed!")
 
     # Log the conclusion of the operations
     print("*** VIDEO DOWNLOADED SUCCESSFULLY ***")
